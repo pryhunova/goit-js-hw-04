4f067b097480490d508d69c55bec3565a77e6e77
Natalie Pryhunova <65974675+pryhunova@users.noreply.github.com>
2021-02-08 02:20:53 +0200

    Create task-10-bind+callback.js

---
+++ js/task-10-bind+callback.js
@@ -0,0 +1,25 @@
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    this.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
+}
+
+const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service));
+console.log(firstInvoke);
+
+console.log(service.mailingList);
+const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service));
+console.log(secondInvoke);
+
+console.log(service.mailingList); 
